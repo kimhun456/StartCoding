370f20f290db676a2a7e551e0318a007a16517e5
hyunjae <kimhun456@gmail.com>
2016-12-05 03:12:27 +0900

    퍼센트 올리기

--- public/javascripts/list.js
+++ public/javascripts/list.js
@@ -5,11 +5,12 @@
 
 $("#loginText").html("로그아웃");
 var elem = document.getElementById("myCBar");
-var width = 10;
-var id = setInterval(frame, 10);
-
+var width = 0;
+var id = setInterval(frame, 100);
+document.getElementById("label").innerHTML = width + '%';
+elem.style.width='0%';
 function frame(){
-    if(width >= 50){
+    if(width >= 10){
         clearInterval(id);
     }else{
         width++;
